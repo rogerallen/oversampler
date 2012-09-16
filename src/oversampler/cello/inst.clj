bfd0b2f82b2d8b89c5dfa03c7f389c39573a25e8
Roger Allen <rallen@gmail.com>
2012-09-16 09:21:54 -0700

    code cleanup.  snapshot before trying cello-init fn

--- src/oversampler/cello/inst.clj
+++ src/oversampler/cello/inst.clj
@@ -19,52 +19,56 @@
 ;; ======================================================================
 ;; several index buffers to use for grabbing per-note control information
 
-(defn- set-buffer-indices
+(defn- fill-buffer-sample-ids
+  "fill 128 sample-ids in buf starting at offset for a particular volume (pp, mf, ff)"
   [buf offset volume]
   (dotimes [i 128]
     (if (bank/get-sample i volume)
       (o/buffer-set! buf (+ offset i) (:id (bank/get-sample i volume))))))
 
 ;; buffer of buffer ids for instrument note ctl to index through
-(defonce ^:private index-buffer
+(defonce ^:private note-to-sample-id-buffer
   (let [buf (o/buffer (* 3 128))]
     (o/buffer-fill! buf (:id silent-buffer))
-    (set-buffer-indices buf 0 raw/pp)
-    (set-buffer-indices buf 128 raw/mf)
-    (set-buffer-indices buf 256 raw/ff)
+    (fill-buffer-sample-ids buf 0 raw/pp)
+    (fill-buffer-sample-ids buf 128 raw/mf)
+    (fill-buffer-sample-ids buf 256 raw/ff)
     buf))
 
-(defn- set-buffer-scaling-factors
+(defn- fill-buffer-scaling-factors
+  "fill 128 level-scaling-factors in buf starting at offset for a particular volume (pp, mf, ff)"
   [buf offset volume]
   (dotimes [i 128]
     (if (bank/get-sample i volume)
-      (o/buffer-set! buf (+ offset i) (/ 2.0 (:ppeak (bank/get-sample-info i volume))))))) ;; ???
+      (o/buffer-set! buf (+ offset i) (/ 2.0 (:ppeak (bank/get-sample-info i volume)))))))
 
 ;; buffer of scaling values for note ctl to index through
-(defonce ^:private sample-scale-buffer
+(defonce ^:private note-to-level-scale-buffer
   (let [buf (o/buffer (* 3 128))]
     (o/buffer-fill! buf 1.0)
-    (set-buffer-scaling-factors buf 0 raw/pp)
-    (set-buffer-scaling-factors buf 128 raw/mf)
-    (set-buffer-scaling-factors buf 256 raw/ff)
+    (fill-buffer-scaling-factors buf 0 raw/pp)
+    (fill-buffer-scaling-factors buf 128 raw/mf)
+    (fill-buffer-scaling-factors buf 256 raw/ff)
     buf))
 
-(defn- set-buffer-lengths
+(defn- fill-buffer-lengths
+  "fill 128 length (in seconds) in buf starting at offset for a particular volume (pp, mf, ff)"
   [buf offset volume]
   (dotimes [i 128]
     (if (bank/get-sample i volume)
       (let [the-info (bank/get-sample-info i volume)
-            the-length (- (:end the-info) (:start the-info))]
-        (o/buffer-set! buf (+ offset i)
-                       (/ (- the-length (* 4 4410)) 44100.0)))))) ;; FIXME for non-44.1kHz
-   
+            the-length (- (:end the-info) (:start the-info))
+            the-rate (:rate (:sample the-info))
+            length-in-secs (/ the-length the-rate)] 
+        (o/buffer-set! buf (+ offset i) length-in-secs)))))
+
 ;; buffer of sample-lengths for note ctl to index through
-(defonce ^:private sample-length-buffer
+(defonce ^:private note-to-length-buffer
   (let [buf (o/buffer (* 3 128))]
     (o/buffer-fill! buf 0.0)
-    (set-buffer-lengths buf 0 raw/pp)
-    (set-buffer-lengths buf 128 raw/mf)
-    (set-buffer-lengths buf 256 raw/ff)
+    (fill-buffer-lengths buf 0 raw/pp)
+    (fill-buffer-lengths buf 128 raw/mf)
+    (fill-buffer-lengths buf 256 raw/ff)
     buf))
 
 ;; buffer of buffer-offsets for level to index through.
@@ -99,15 +103,15 @@
 (o/definst sampled-cello
   [note 60 level 1 rate 1 loop? 0
    attack 0 decay 1 sustain 1 release 0.2 curve -4 gate 1]
-  (let [ofst (o/index:kr (:id level-to-offset-buffer) (o/floor (* 20 level))) ;; FIXME -- (o/max 0.0 (o/min 1.0 level)))))
-        buf (o/index:kr (:id index-buffer) (+ ofst note))
-        the-sample-scale (o/index:kr (:id sample-scale-buffer) (+ ofst note))
-        the-sample-length (o/index:kr (:id sample-length-buffer) (+ ofst note))
+  (let [ofst (o/index:kr (:id level-to-offset-buffer) (o/floor (* 20 level))) ;; FIXME? -- (o/max 0.0 (o/min 1.0 level)))))
+        the-sample-id (o/index:kr (:id note-to-sample-id-buffer) (+ ofst note))
+        the-sample-scale (o/index:kr (:id note-to-level-scale-buffer) (+ ofst note))
+        the-sample-length (o/index:kr (:id note-to-length-buffer) (+ ofst note))
         ;; envelope for making sure we have zeros at start/end, scale height, too.
-        env2 (o/env-gen (o/envelope [0 0 1 1 0 0] [0.01 0.01 the-sample-length 0.01 0.01] :sine)
+        env2 (o/env-gen (o/envelope [0 0 1 1 0 0] [0.01 0.01 (- the-sample-length 0.04) 0.01 0.01] :sine)
                         :level-scale the-sample-scale
                         :gate gate :action o/FREE)
         ;; regular adsr envelope
         env (o/env-gen (o/adsr attack decay sustain release level curve)
                        :gate gate :action o/FREE)]
-    (* level env2 env (o/scaled-play-buf 1 buf :level 1.0 :loop loop? :action o/FREE))))
+    (* level env2 env (o/scaled-play-buf 1 the-sample-id :level 1.0 :loop loop? :action o/FREE))))

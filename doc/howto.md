e0e2e19a1e7d68970a9d95d0eec1eebf739d0d20
Roger Allen <rallen@gmail.com>
2013-02-09 09:54:46 -0800

    initial steps in documenting process & adding guitar.

---
+++ doc/howto.md
@@ -0,0 +1,9 @@
+# How to add a sampled-instrument to Oversampler
+
+1. download samples locally
+2. create structure to hold new instrument
+3. process the samples to gather metadata
+   a. automatic processing
+   b. manual processing
+4. final cleanup
+
--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
-(defproject oversampler "0.3.0"
+(defproject oversampler "0.4.0-SNAPSHOT"
   :description "overtone sampler library"
   :url "https://github.com/rogerallen/oversampler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [[org.clojure/clojure "1.4.0"]
-   [overtone "0.8.0"]
-   ;; When doing analysis, uncomment the Incanter library and the analysis use line in core.clj
-   ;; When deploying, comment it out.
-   ;; The reason is that Incanter is not necessary for playing samples and can sometimes
-   ;; cause troubles (I ran into some odd conflict with quil, for example).
-   ;; {and this seems better than having 2 repositories.  at least for now}
-   ;;[incanter "1.2.3-SNAPSHOT"]
+   [overtone "0.8.1"]
+   ;; When doing analysis, uncomment the Incanter library and the
+   ;; analysis use line in core.clj When deploying, comment it out.
+   ;; The reason is that Incanter is not necessary for playing samples
+   ;; and can sometimes cause troubles (I ran into some odd conflict
+   ;; with quil, for example).  {and this seems better than having 2
+   ;; repositories.  at least for now}
+   [incanter "1.4.2"] ;; was 1.2.3-SNAPSHOT
+   ;; Another library for analysis.
+   [criterium "0.3.1"]
    ]
-  :plugins [ [lein-swank "1.4.4"] ]
   ;; had to comment this out before "lein install" would work
   ;;:main ^{:skip-aot true} oversampler.core
   :test-selectors {:quick :quick}

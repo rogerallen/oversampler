098c98f0edce90c32baa168c3fa5a83c48b8a100
Roger Allen <rallen@gmail.com>
2014-03-10 21:17:07 -0700

    go to overtone 0.9.1

--- project.clj
+++ project.clj
@@ -5,18 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [[org.clojure/clojure "1.4.0"]
-   [overtone "0.9.0-SNAPSHOT"]
-   ;; When doing analysis, uncomment the Incanter library and the
-   ;; analysis use line in core.clj When deploying, comment it out.
-   ;; The reason is that Incanter is not necessary for playing samples
-   ;; and can sometimes cause troubles (I ran into some odd conflict
-   ;; with quil, for example).  {and this seems better than having 2
-   ;; repositories.  at least for now}
-   [incanter "1.4.1"] ;; was 1.2.3-SNAPSHOT
-   ]
-  :test-selectors {:quick :quick
-                   :cello :cello
+   [overtone "0.9.1"]]
+  :test-selectors {:quick  :quick
+                   :cello  :cello
                    :ektara :ektara}
   ;; okay 128M is not enough when you have 40M files
   :jvm-opts [ "-Xms512m" "-Xmx1g" ]
+  ;; use profile for analysis
+  :profiles {:dev {:dependencies [[incanter "1.4.1"]]}}
   )

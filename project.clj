852e6fbd7f10118bfd4f4ecca39a20a5a978dd61
Roger Allen <rallen@gmail.com>
2014-03-10 22:28:36 -0700

    modern clojure

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
-  [[org.clojure/clojure "1.4.0"]
+  [[org.clojure/clojure "1.5.1"]
    [overtone "0.9.1"]]
   :test-selectors {:quick  :quick
                    :cello  :cello
